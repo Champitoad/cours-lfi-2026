181ac29a56c79563db3ef4798887dd22aaa8fb14
Pablo Donato <pablo.donato@tuta.io>
2026-03-24 11:41:09 +0100

    Add more material for lesson 2

--- lesson2/exos.v
+++ lesson2/exos.v
@@ -1,127 +1,240 @@
-(* ================================================================= *)
-(* Leçon 2 : Logique Propositionnelle et Constructivisme             *)
-(* ================================================================= *)
+(** * LEÇON 2 : Logique Propositionnelle et Constructivisme *)
 
-(* Lors de la première leçon, nous avons vu l'implication (->).
-   Aujourd'hui, nous allons introduire la conjonction (ET), 
-   la disjonction (OU), et la négation (NON). *)
+(** Après avoir exploré l'implication (->), notre voyage au cœur de la correspondance
+    de Curry-Howard se poursuit. Aujourd'hui, nous allons découvrir comment Rocq
+    traite la Conjonction (ET), la Disjonction (OU), et l'Absurdité (NON).
 
-Section Lecon2.
-Variables P Q R : Prop.
+    Nous adopterons une perspective profondément constructiviste (la sémantique
+    Brouwer-Heyting-Kolmogorov, ou BHK), où chaque connecteur logique n'est pas 
+    une simple table de vérité, mais une véritable *structure de données* 
+    informatique construite par nos soins ! *)
 
-(* ================================================================= *)
-(* PARTIE 1 : La Conjonction (Le ET : /\ )                           *)
-(* ================================================================= *)
+(** Ajoutons une notation agréable pour écrire nos preuves de conjonction 
+    directement sous forme de paire mathématique (x, y). *)
+Notation "( x , y )" := (conj x y).
+(** On fait de même pour les preuves de disjonction avec les injections ι₁ et ι₂. *)
+Notation "'ι₁'" := or_introl (at level 0).
+Notation "'ι₂'" := or_intror (at level 0).
 
-(* Pour prouver 'P /\ Q', nous devons fournir deux choses : 
-   une preuve de P, ET une preuve de Q. 
-   La tactique pour diviser ce but en deux est 'split'. *)
+Section Lesson2.
 
-Lemma ET_intro : P -> Q -> P /\ Q.
+Variables A B C : Prop.
+
+(** * PARTIE 1 : LA CONJONCTION (Le ET : /\) *)
+
+(** Posséder une preuve formelle de [A /\ B], c'est posséder indépendamment une
+    preuve de [A] ET une preuve de [B]. Informatiquement, cela correspond exactement
+    à la notion de *paire* (ou "tuple").
+
+    Pour construire/introduire une telle paire, nous utilisons la tactique [split]. *)
+Lemma ET_intro : A -> B -> A /\ B.
 Proof.
-  intros p q.
+  intros x y.
+  (** Notre but est [A /\ B]. Nous avons besoin d'en fournir les deux composantes. *)
   split.
-  - exact p. (* Le tiret '-' permet de se concentrer sur le 1er sous-but *)
-  - exact q. (* Et ici sur le 2ème sous-but *)
+  (** [split] a divisé notre but en deux. Remarquez l'usage de la puce sous 
+      forme de tiret '-' qui permet de se concentrer successivement sur le
+      premier puis second buts générés. *)
+  - exact x.
+  - exact y.
 Qed.
 
-(* Si nous AVONS un 'P /\ Q' dans nos hypothèses, comment l'utiliser ?
-   Nous devons le "détruire" pour en extraire les deux composants.
-   La tactique est 'destruct'. *)
+(** Observons le terme final. Le compilateur nous montre que le terme
+    de preuve est bel et bien structuré comme une paire ! *)
+Print ET_intro.
 
-Lemma ET_symetrie : P /\ Q -> Q /\ P.
+(** À l'inverse, si nous *avons* une hypothèse de type [A /\ B], nous pouvons
+    la "détruire" pour en extraire les deux composants sous-jacents, grâce à 
+    la commande [destruct]. Cela correspond aux "projections" π₁ et π₂. *)
+Lemma ET_symetrie : A /\ B -> B /\ A.
 Proof.
-  intro H.
-  destruct H as [p q]. (* Extrait les preuves p:P et q:Q de l'hypothèse H *)
+  intros H_ab.
+  (** L'hypothèse [H_ab] cache une paire. [destruct] va l'ouvrir et nommer
+      ses deux composants [x] (la preuve de A) et [y] (la preuve de B). *)
+  destruct H_ab as [x y].
+  Show Proof.
   split.
-  - exact q.
-  - exact p.
+  - exact y.
+  - exact x.
 Qed.
 
-(* ================================================================= *)
-(* PARTIE 2 : La Disjonction (Le OU : \/ )                           *)
-(* ================================================================= *)
+(** * PARTIE 2 : LA DISJONCTION (Le OU : \/ ) *)
 
-(* L'approche de Rocq est CONSTRUCTIVISTE. Pour prouver 'P \/ Q', 
-   vous DEVEZ prouver P, ou bien prouver Q. 
-   Les tactiques sont 'left' (pour choisir de prouver le côté gauche) 
-   et 'right' (pour choisir le côté droit). *)
+(** Voici le grand point de rupture du constructivisme ! Pour prouver [A \/ B],
+    vous ne pouvez pas joyeusement affirmer "bon, l'un des deux doit être vrai",
+    sans savoir lequel. Vous DEVEZ exhiber consciemment une preuve ou bien de [A],
+    ou bien de [B].
 
-Lemma OU_intro_gauche : P -> P \/ Q.
+    Informatiquement, c'est ce qu'on appelle un type "somme". L'ordinateur conserve 
+    la trace indélébile du choix effectué via des fonctions d'injection : 
+    [left] correspond à ι₁ et [right] correspond à ι₂. *)
+Lemma OU_intro_gauche : A -> A \/ B.
 Proof.
-  intro p.
-  left. (* "Je choisis de prouver P, car j'ai une preuve de P !" *)
-  exact p.
+  intros x.
+  (** Nous faisons le choix fort de prouver le côté gauche en injectant [x]. *)
+  left.
+  exact x.
 Qed.
 
-(* Pour utiliser un OU qui est dans nos hypothèses, on utilise 
-   aussi 'destruct'. Cela va créer deux mondes parallèles : 
-   un monde où P est vrai, et un monde où Q est vrai. *)
+(** On constate que le terme de preuve final est l'application de l'injection 
+    gauche [ι₁] à la variable [x] de type [A].
 
-Lemma OU_symetrie : P \/ Q -> Q \/ P.
+    On note aussi que le type [B] est passé en argument juste avant [x] : c'est
+    parce que Rocq a besoin de stocker le type de la branche inutilisée (ici à droite)
+    pour connaître le type complet [A \/ B] de l'injection, et cette information ne
+    peut être inférée uniquement à partir du type de [x]. *)
+Print OU_intro_gauche.
+
+(** À l'inverse, l'injection droite avec la tactique [right] : *)
+Lemma OU_intro_droite : B -> A \/ B.
 Proof.
+  intros y.
+  right.
+  exact y.
+Qed.
+
+(** De même ici, le terme de preuve est l'application de l'injection droite [ι₂]
+    à la variable [y]. *)
+Print OU_intro_droite.
+
+(** DISTINCTION FONDAMENTALE : DONNÉES vs CONTRÔLE
+
+    En informatique, on sépare généralement deux grands piliers :
+    1. Les Structures de Données : comment la machine représente et stocke 
+       l'information (ex: les paires pour le ET, ou les injections pour le OU).
+    2. Les Structures de Contrôle : comment la machine prend des décisions 
+       et oriente le flux du calcul en se basant sur ces données.
+
+    Posséder une disjonction [A \/ B] en hypothèse garantit qu'un choix a été
+    fait (injection gauche ou droite), mais cela forme une structure de donnée
+    "fermée", car on ne sait pas a priori quel choix a été fait. Pour l'exploiter
+    activement dans notre preuve/programme, nous avons besoin d'une structure de
+    contrôle pour "ouvrir" cette boîte et faire bifurquer notre raisonnement.
+
+    La structure de contrôle la plus célèbre est le "if... then... else...".
+    Sa généralisation s'appelle le "Filtrage par Motif" (ou [match ... with]). 
+    C'est une sorte d'aiguillage, qui correspond en fait au raisonnement par cas :
+    "SI la boîte contient une preuve de A, ALORS applique ce premier raisonnement;
+    SI elle contient une preuve de B, ALORS applique cet autre raisonnement". 
+    
+    C'est exactement ce que génère la tactique [destruct] pour nous ! *)
+Lemma OU_symetrie : A \/ B -> B \/ A.
+Proof.
+  intros H_ou.
+  (** Appliquer un [destruct] avec la barre séparatrice '|' génère deux univers 
+      parallèles à gérer individuellement : l'univers où une valeur de type A avait
+      été injectée, et l'univers où une valeur de type B avait été injectée. *)
+  destruct H_ou as [x | y].
+  (** Comme avec la tactique [split], Rocq nous génère deux "sous-buts", un pour
+      chaque univers. *)
+  - (* Cas 1 : La preuve d'origine était une injection gauche, pour A *)
+    right. exact x.
+  - (* Cas 2 : La preuve d'origine était une injection droite, pour B *)
+    left. exact y.
+Qed.
+
+(** Remarquez le terme compilé final : ce n'est rien d'autre qu'un [match] ! *)
+Print OU_symetrie.
+
+(** * PARTIE 3 : L'ABSURDITÉ ET LA NÉGATION *)
+
+(** Dans un monde constructiviste, la définition de la négation est d'une
+    élégance presque poétique : [~A] est exactement une *abréviation* pour [A -> False].
+    Prouver que A est faux, c'est tout bêtement écrire une fonction qui,
+    si elle recevait une preuve de A, réussirait à cracher l'absurdité [False].
+    
+    Et qu'est-ce que [False] ? C'est le type ultimement vide : de par sa conception,
+    il n'a aucun constructeur, donc aucun habitant ! (sinon notre système logique
+    deviendrait trivial à cause du principe d'explosion) *)
+Lemma non_contradiction : ~(A /\ ~A).
+Proof.
+  (** Gardez en tête que le but est en fait de prouver [(A /\ (A -> False)) -> False] *)
+  intros H_impossible.
+  destruct H_impossible as [a non_a].
+  (** Nous avons en main [a : A] et une étrange fonction [non_a : A -> False].
+      Il suffit de faire s'entrechoquer les deux... Modus Ponens ! *)
+  apply non_a.
+  exact a.
+Qed.
+
+(** Le Principe d'Explosion (Ex Falso Quodlibet).
+    Si vous obtenez une preuve de [False] dans votre contexte, c'est donc que vous
+    avez au moins deux hypothèses contradictoires [A] et [~A]. Et d'une contradiction,
+    vous pouvez déduire N'IMPORTE QUOI. En Rocq, c'est la tactique [exfalso]. *)
+Lemma explosion : False -> A.
+Proof.
+  intro f.
+  exfalso.
+  exact f.
+Qed.
+
+(** * PARTIE 4 : LA CRISE DU CONSTRUCTIVISME ET LE TIERS-EXCLU *)
+
+(** L'idéalisme intuitionniste a un revers : le Tiers-Exclu [A \/ ~A] n'est
+    pas prouvable de manière générique. C'est logique : réussir à le prouver 
+    reviendrait à posséder l'Algorithme Universel capable de trancher la vérité 
+    de n'importe quelle proposition A sans calcul préalable. Et nous savons tous 
+    que ce rêve du mathématicien David Hilbert a été ruiné par le Théorème d'Incomplétude
+    de  Gödel et le problème de l'Arrêt de Turing. *)
+Lemma tiers_exclu_fail : A \/ ~A.
+Proof.
+  (** On peut esquisser une tentative :
+      Si on essaye [left], l'ordinateur attendra de nous une preuve de A... *)
+  left.
+  (** ... que l'on n'a pas. Annulons avec la commande [Undo]. *)
+  Undo.
+  (** Si on essaye [right], l'ordinateur attendra qu'on réfute A. *)
+  right.
   intro H.
-  destruct H as [p | q]. (* La barre '|' sépare les deux cas possibles *)
-  - (* Cas 1 : On suppose que c'était P *)
-    right. exact p.
-  - (* Cas 2 : On suppose que c'était Q *)
-    left. exact q.
+  (** Enfermés et toujours ignorants du problème précis sous-jacent à la proposition
+      abstraite A, nous ne pouvons que renoncer lâchement avec la commande [Abort]. *)
+Abort.
+
+(** Conséquence : si l'on veut retrouver notre zone de confort mathématique 
+    (la logique classique et platonicienne de Descartes/Aristote), on doit forcer le calcul 
+    en imposant violemment le principe de Tiers-Exclu comme une vérité infuse via la 
+    commande [Axiom]. *)
+Axiom LEM : forall X : Prop, X \/ ~X.
+
+(** Muni de cette arme redoutable, un nombre vertigineux de théorèmes inaccessibles 
+    sur un plan constructiviste (car incomputables) s'offrent de nouveau à nous.
+    La porte étendard de la pensée classique est sans conteste l'Élimination de la 
+    Double Négation : (Non-(Non A) alors A). *)
+Lemma double_negation : ~~A -> A.
+Proof.
+  intros non_non_a.
+  (** Pour arracher [A] au néant, on invoque le Tiers-Exclu [LEM] en l'instanciant 
+      avec [A]. Ensuite, [destruct] va de nouveau séparer les univers. *)
+  destruct (LEM A) as [x | non_a].
+  - (* L'univers 1 correspond au cas où (par magie cosmique), A était vrai. 
+       On a A à disposition, la victoire est immédiate. *)
+    exact x.
+  - (* L'univers 2 correspond au cas où (par magie cosmique), ~A était vrai.
+       Mais attendez! Notre hypothèse constructiviste de départ [non_non_a] nous
+       jure la main sur le cœur que [~A -> False] ! La contradiction est totale. *)
+    exfalso.
+    apply non_non_a.
+    exact non_a.
 Qed.
 
-(* ================================================================= *)
-(* PARTIE 3 : La Négation et la Crise Constructiviste                *)
-(* ================================================================= *)
+(** * PARTIE 5 : EXERCICES À FAIRE *)
 
-(* Dans Rocq, la négation '~P' est juste une abréviation pour 'P -> False'. 
-   Prouver '~P', c'est prouver que supposer P mène à une absurdité. *)
-
-Lemma non_contradiction : ~(P /\ ~P).
+(** EXERCICE 1 : Prouver formellement un côté des fameuses Lois de De Morgan.
+    Ce sens-ci est tout à fait harmonieux avec la pensée constructiviste 
+    (NUL BESOIN d'utiliser le Tiers-Exclu [LEM], que des règles habituelles !). *)
+Lemma de_morgan_1 : (A \/ B) -> ~(~A /\ ~B).
 Proof.
-  intro H.
-  destruct H as [p non_p].
-  (* Nous avons 'p : P' et 'non_p : P -> False'. Modus Ponens ! *)
-  apply non_p.
-  exact p.
-Qed.
+  (* ÉCRIVEZ VOTRE PREUVE ICI *)
+Admitted. (* N'oubliez pas de remplacer par Qed. quand le compilateur est content
+             (ou plutôt, pas pas content...) *)
 
-(* Mais que se passe-t-il si nous essayons de prouver le principe 
-   du Tiers Exclu (Tout est soit vrai, soit faux) à partir de rien ? *)
-
-Lemma tiers_exclu_fail : P \/ ~P.
+(** EXERCICE 2 : Prouvez enfin sa monstrueuse réciproque !
+    Attention : dans ce sens, le constructivisme est paralysé. Vous n'aurez d'autre choix
+    que de brandir le Tiers Exclu en effectuant des raisonnements par cas 
+    sur [A \/ ~A] ou [B \/ ~B] pour espérer craquer la coquille et conclure. *)
+Lemma de_morgan_2 : ~(~A /\ ~B) -> (A \/ B).
 Proof.
-  (* Essayons... Si je fais 'left', je dois prouver P. Mais je n'ai rien !
-     Si je fais 'right', je dois prouver ~P. Mais je n'ai rien non plus ! 
-     Dans un monde constructif, on ne peut pas affirmer P \/ ~P sans 
-     savoir concrètement lequel des deux est vrai. *)
-Abort. (* 'Abort' annule la preuve en cours. *)
+  (* ÉCRIVEZ VOTRE PREUVE ICI *)
+Admitted.
 
-(* ================================================================= *)
-(* PARTIE 4 : L'Axiome et la Logique Classique                       *)
-(* ================================================================= *)
-
-(* Pour faire des mathématiques classiques, nous devons imposer 
-   le Tiers Exclu comme une loi universelle. 
-   Nous utilisons la commande 'Axiom'. *)
-
-Axiom LEM : forall A : Prop, A \/ ~A.
-
-(* L'univers logique vient de changer ! 
-   Nous pouvons maintenant prouver l'élimination de la double négation, 
-   qui est impossible en logique purement constructiviste. *)
-
-Lemma double_negation : ~~P -> P.
-Proof.
-  intro non_non_p.
-  (* Nous invoquons notre axiome divin pour séparer l'univers en deux *)
-  destruct (LEM P) as [p | non_p].
-  - (* Cas 1 : P est vrai. Facile ! *)
-    exact p.
-  - (* Cas 2 : P est faux (~P). 
-       Mais on nous a donné hypothèse ~~P (c'est-à-dire ~P -> False). *)
-    (* Nous avons une contradiction entre non_p et non_non_p ! *)
-    exfalso. (* "De cette absurdité, je peux tout déduire" *)
-    apply non_non_p.
-    exact non_p.
-Qed.
-
-End Lecon2.
+End Lesson2.
---
+++ lesson2/exos_bonus.v
@@ -0,0 +1,480 @@
+(****************************************************************)
+(* Exercices bonus : Toutes les tautologies d'Edukera           *)
+(*                                                              *)
+(* Remplacez 'Admitted.' par 'Qed.' une fois la preuve finie.   *)
+(* Attention : certaines (ex: Classical logic) nécessitent le   *)
+(* Tiers-Exclu (LEM) ! Si besoin, déclarez l'axiome LEM.        *)
+(****************************************************************)
+
+Section Tautologies.
+
+Variables (A B C D : Prop).
+
+(** # Connectives *)
+
+(** ## Tutorial *)
+
+(** ### Implication *)
+
+Lemma tautologie_1 :
+  A -> B -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_1.
+
+Lemma tautologie_2 :
+  A -> (A -> B) -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_2.
+
+Lemma tautologie_3 :
+  (B -> C) -> (A -> B) -> (A -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_3.
+
+
+(** ### Conjunction *)
+
+Lemma tautologie_4 :
+  A -> B -> A /\ B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_4.
+
+Lemma tautologie_5 :
+  A -> B -> C -> A /\ (B /\ C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_5.
+
+Lemma tautologie_6 :
+  (A /\ B) -> (C -> A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_6.
+
+Lemma tautologie_7 :
+  (A /\ B) -> (C -> B).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_7.
+
+Lemma tautologie_8 :
+  (A /\ B) -> (B /\ A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_8.
+
+
+(** ### Disjunction *)
+
+Lemma tautologie_9 :
+  A -> A \/ B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_9.
+
+Lemma tautologie_10 :
+  B -> A \/ B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_10.
+
+Lemma tautologie_11 :
+  B -> A \/ (B \/ C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_11.
+
+Lemma tautologie_12 :
+  A \/ B -> (A -> B) -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_12.
+
+Lemma tautologie_13 :
+  A \/ B -> (B \/ A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_13.
+
+
+(** ### Negation *)
+
+Lemma tautologie_14 :
+  False -> ~ A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_14.
+
+Lemma tautologie_15 :
+  ~ A -> A -> False.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_15.
+
+Lemma tautologie_16 :
+  A -> ~ ~ A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_16.
+
+Lemma tautologie_17 :
+  False -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_17.
+
+Lemma tautologie_18 :
+  ~ A -> A -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_18.
+
+Lemma tautologie_19 :
+  A -> (C -> ~ B) -> (A -> B) -> ~ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_19.
+
+Lemma tautologie_20 :
+  (A -> B) /\ (A -> ~ B) -> ~ A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_20.
+
+Lemma tautologie_21 :
+  (A \/ ~ B) /\ B -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_21.
+
+Lemma tautologie_22 :
+  ~ A \/ B -> A -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_22.
+
+Lemma tautologie_23 :
+  (A -> B) -> (~ B -> ~ A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_23.
+
+
+(** ## Exercises *)
+
+(** ### Constructive logic *)
+
+Lemma tautologie_24 :
+  A /\ B -> C -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_24.
+
+Lemma tautologie_25 :
+  A /\ B -> C -> B /\ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_25.
+
+Lemma tautologie_26 :
+  A -> A /\ (A \/ B).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_26.
+
+Lemma tautologie_27 :
+  A \/ (B /\ C) -> A \/ B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_27.
+
+Lemma tautologie_28 :
+  A \/ B -> (A -> B) -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_28.
+
+Lemma tautologie_29 :
+  (A -> B -> C) -> ((A -> B) -> A -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_29.
+
+Lemma tautologie_30 :
+  (A -> B -> C) -> A /\ B -> C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_30.
+
+Lemma tautologie_31 :
+  (A -> B) -> (A -> C) -> A -> B /\ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_31.
+
+Lemma tautologie_32 :
+  (A -> B) -> A \/ C -> B \/ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_32.
+
+Lemma tautologie_33 :
+  (A /\ B) \/ (A \/ B) -> A \/ B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_33.
+
+Lemma tautologie_34 :
+  (A -> C) /\ (B -> C) -> A \/ B -> C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_34.
+
+Lemma tautologie_35 :
+  ~ A \/ B -> A -> B.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_35.
+
+Lemma tautologie_36 :
+  (A -> B -> C) -> A /\ B -> C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_36.
+
+Lemma tautologie_37 :
+  A -> (A -> B) -> ((A -> B) -> (B -> C)) -> C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_37.
+
+Lemma tautologie_38 :
+  (A /\ B -> C) <-> A -> B -> C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_38.
+
+Lemma tautologie_39 :
+  (A \/ B -> C) <-> (A -> C) /\ (B -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_39.
+
+
+(** ### Associativity *)
+
+Lemma tautologie_40 :
+  A /\ (B /\ C) <-> (A /\ B) /\ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_40.
+
+Lemma tautologie_41 :
+  A \/ (B \/ C) <-> (A \/ B) \/ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_41.
+
+
+(** ### Distributivity *)
+
+Lemma tautologie_42 :
+  A /\ (B -> C) -> A /\ B -> A /\ C.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_42.
+
+Lemma tautologie_43 :
+  A /\ (B \/ C) <-> (A /\ B) \/ (A /\ C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_43.
+
+Lemma tautologie_44 :
+  A \/ (B /\ C) <-> (A \/ B) /\ (A \/ C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_44.
+
+Lemma tautologie_45 :
+  (A -> B) /\ (A -> C) -> (A -> (B /\ C)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_45.
+
+Lemma tautologie_46 :
+  (A -> (B /\ C)) -> (A -> B) /\ (A -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_46.
+
+Lemma tautologie_47 :
+  (A -> B) \/ (A -> C) -> (A -> (B \/ C)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_47.
+
+Lemma tautologie_48 :
+  (A -> (B -> C)) -> ((A -> B) -> (A -> C)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_48.
+
+Lemma tautologie_49 :
+  ((A -> B) -> (A -> C)) -> (A -> (B -> C)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_49.
+
+Lemma tautologie_50 :
+  (A \/ B -> C) <-> (A -> C) /\ (B -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_50.
+
+
+(** ### Classical logic *)
+
+Axiom LEM : forall A, A \/ ~A.
+
+Lemma tautologie_51 :
+  (A /\ B -> A /\ C) -> A /\ (B -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_51.
+
+Lemma tautologie_52 :
+  (A -> (B \/ C)) -> (A -> B) \/ (A -> C).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_52.
+
+
+Lemma tautologie_53 :
+  A \/ ~ A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_53.
+
+Lemma tautologie_54 :
+  A \/ (A -> B).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_54.
+
+Lemma tautologie_55 :
+  (A -> B) \/ (B -> A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_55.
+
+Lemma tautologie_56 :
+  ~ ~ A -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_56.
+
+Lemma tautologie_57 :
+  ((A -> B) -> A) -> A.
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_57.
+
+
+Lemma tautologie_58 :
+  ~ ~ (A \/ ~ A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_58.
+
+Lemma tautologie_59 :
+  ~ ~ (A \/ (A -> B)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_59.
+
+Lemma tautologie_60 :
+  ~ ~ ((A -> B) \/ (B -> A)).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_60.
+
+Lemma tautologie_61 :
+  ~ ~ (~ ~ A -> A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_61.
+
+Lemma tautologie_62 :
+  ~ ~ (((A -> B) -> A) -> A).
+Proof.
+  (* À compléter *)
+Admitted.
+Print tautologie_62.
+
+End Tautologies.
